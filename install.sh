a3fcf238cee70a38c19b6ab4e6294bca2e1cf00d
Franco Grobler <franco@grobler.fyi>
2024-12-12 10:14:42 +0200

    feat(install): update install script
    Add commet to message added to zshrc file

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
-#!/bin/bash 
+#!/usr/bin/env bash
 
 # Check if zshrc is source by ~/.zshrc
 config="source $HOME/.config/zsh/zshrc"
+read -r -d '' message << EOM
+# Dotfiles ZSH config
+${config}
+EOM
 zshrc="$HOME/.zshrc"
-grep -qxF "$config" "$zshrc" || echo "$config" >> "$zshrc"
+grep -qxF "$config" "$zshrc" || echo "$message" >> "$zshrc"
 
 stow .
