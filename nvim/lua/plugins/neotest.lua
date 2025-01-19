21c7a46adbc34b35b33c11455867d34bd248152a
Franco Grobler <franco@grobler.fyi>
2025-01-19 14:48:58 +0200

    feat(nvim): leetcode & golang test updates
    Split leetcode into seperate file
    Add neotest-golang

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,10 +1,13 @@
 ---@type LazySpec
 return {
   "nvim-neotest/neotest",
+  dependencies = {
+    { "fredrikaverpil/neotest-golang", version = "*" },
+  },
   opts = function()
     return {
       adapters = {
-        -- require "neotest-golang",
+        require "neotest-golang",
         -- require "neotest-rust",
         require "neotest-python",
       },
