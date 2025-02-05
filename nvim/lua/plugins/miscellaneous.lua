089c12739f27c2316fac642270b20e72965da67d
Franco Grobler <franco@grobler.fyi>
2025-02-05 13:32:43 +0200

    fix(nvim): use astrocore mappings for fuck my life

--- nvim/lua/plugins/miscellaneous.lua
+++ nvim/lua/plugins/miscellaneous.lua
@@ -1,11 +1,30 @@
 return {
   {
     "eandrju/cellular-automaton.nvim",
-    config = function()
-      require "cellular-automaton"
-      vim.keymap.set("n", "<leader>AR", "<cmd>CellularAutomaton make_it_rain<CR>")
-      vim.keymap.set("n", "<leader>AG", "<cmd>CellularAutomaton game_of_life<CR>")
-    end,
+    dependecies = {
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          mappings = {
+            n = {
+              ["A"] = { desc = "Fuck my life" },
+              ["AG"] = {
+                function() require("cellular-automaton").animations.make_it_rain() end,
+                desc = "Make it rain",
+              },
+              ["AR"] = {
+                function() require("cellular-automaton").animations.game_of_life() end,
+                desc = "Game of life",
+              },
+              ["AS"] = {
+                function() require("cellular-automaton").animations.scramble() end,
+                desc = "Scramble",
+              },
+            },
+          },
+        },
+      },
+    },
   },
   "preservim/vim-pencil",
 }
