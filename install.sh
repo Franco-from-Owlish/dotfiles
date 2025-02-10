a0ae0affed64e8ef0a1a8784e65b663f180d86cd
Franco Grobler <franco@grobler.fyi>
2025-02-10 09:38:56 +0200

    feat(sops): add age key generation during install

--- install.sh
+++ install.sh
@@ -2,11 +2,19 @@
 
 # Check if zshrc is source by ~/.zshrc
 config="source $HOME/.config/zsh/zshrc"
-read -r -d '' message << EOM
+read -r -d '' message <<EOM
 # Dotfiles ZSH config
 ${config}
 EOM
 zshrc="$HOME/.zshrc"
-grep -qxF "$config" "$zshrc" || echo "$message" >> "$zshrc"
+grep -qxF "$config" "$zshrc" || echo "$message" >>"$zshrc"
+
+# Create SOPS age key
+sops_age_dir="sops/age"
+if [[ ! -d ${sops_age_dir} ]]; then
+  mkdir -p "${sops_age_dir}"
+  age-keygen -o "${sops_age_dir}/keys.txt" || true
+  echo "*" >>"${sops_age_dir}/.gitignore"
+fi
 
 stow .
