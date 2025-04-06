78e00465d8b763e4f948447a0173a450ea2f7593
Franco Grobler <franco@grobler.fyi>
2025-04-06 16:44:19 +0200

    feat(nvim): update venv selector keymaps

--- nvim/lua/plugins/venv-selector.lua
+++ nvim/lua/plugins/venv-selector.lua
@@ -9,8 +9,8 @@ return {
   event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
   keys = {
     -- Keymap to open VenvSelector to pick a venv.
-    { "<leader>vs", "<cmd>VenvSelect<cr>", desc = "VenvSelector" },
+    { "<leader>cvs", "<cmd>VenvSelect<cr>", desc = "VenvSelector" },
     -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-    { "<leader>vc", "<cmd>VenvSelectCached<cr>", desc = "VenvSelector" },
+    { "<leader>cvc", "<cmd>VenvSelectCached<cr>", desc = "VenvSelector" },
   },
 }
