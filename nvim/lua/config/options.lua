36456df1958d5a900b87bbaf42fa12df4a76153b
Franco Grobler <franco@grobler.fyi>
2025-04-06 16:16:11 +0200

    feat(nvim): migrate to lazyvim

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,15 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+opt.wrap = true -- Enable line wrap
+opt.mouse = ""
+
+local g = vim.g
+
+g.lazyvim_python_lsp = "basedpyright"
+
+-- VimTex
+g.vimtex_view_method = "skim"
