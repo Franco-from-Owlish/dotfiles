8ac13bef8b935d16ac240f693988d2766e70d19b
Franco Grobler <franco@grobler.fyi>
2025-06-11 23:05:29 +0200

    feat(nix): handle all homebrew config in homemanager

--- nix/users/francogrobler/darwin.nix
+++ nix/users/francogrobler/darwin.nix
@@ -3,6 +3,7 @@
 {
   homebrew = {
     enable = true;
+    brews = [ "mas" ];
     casks = [
       "1password"
       "adobe-acrobat-reader"
@@ -15,14 +16,34 @@
     ];
     caskArgs = {
       appdir = "$HOME/Applications";
+      language = "en-ZA,en-GB";
       require_sha = true;
+    };
+    global = {
+      autoUpdate = true;
+      brewfile = true;
+
+    };
+    masApps = {
+      "1Password for Safari" = 1569813296;
+      Xcode = 497799835;
+    };
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "uninstall";
+      upgrade = true;
     };
   };
 
   # Required for some settings like homebrew to know what user to apply to.
   system.primaryUser = "francogrobler";
 
-  security.pam.services.sudo_local.touchIdAuth = true;
+  security.pam.services.sudo_local = {
+    enable = true;
+    reattach = true;
+    touchIdAuth = true;
+    watchIdAuth = true;
+  };
 
   # The user should already exist, but we need to set this up so Nix knows
   # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
