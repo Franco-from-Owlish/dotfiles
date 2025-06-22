3003860c229b9f7c63f272088511cf10cdcbf817
Franco Grobler <franco@grobler.fyi>
2025-06-22 11:56:16 +0200

    refactor: cleanup config set in home manager

--- install.sh
+++ install.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
 
-# Check if zshrc is source by ~/.zshrc
-config="source $HOME/.config/zsh/zshrc"
-read -r -d '' message <<EOM
-# Dotfiles ZSH config
-${config}
-EOM
-zshrc="$HOME/.zshrc"
-grep -qxF "$config" "$zshrc" || echo "$message" >>"$zshrc"
-
 # Create SOPS age key
 sops_age_dir="sops/age"
 if [[ ! -d ${sops_age_dir} ]]; then
-  mkdir -p "${sops_age_dir}"
-  age-keygen -o "${sops_age_dir}/keys.txt" || true
-  echo "*" >>"${sops_age_dir}/.gitignore"
+	mkdir -p "${sops_age_dir}"
+	age-keygen -o "${sops_age_dir}/keys.txt" || true
+	echo "*" >>"${sops_age_dir}/.gitignore"
 fi
 
 stow .
