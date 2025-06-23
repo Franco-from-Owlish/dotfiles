39d9eb9cae7059562549129830fd29e8d3bb8ad8
Franco Grobler <franco@grobler.fyi>
2025-06-23 22:51:57 +0200

    feat(nix): add some system configs to darwin

--- nix/users/francogrobler/darwin.nix
+++ nix/users/francogrobler/darwin.nix
@@ -44,9 +44,57 @@
     watchIdAuth = true;
   };
 
+  system = {
+    defaults = {
+      finder = {
+        AppleShowAllExtensions = true;
+        FXPreferredViewStyle = "Nlsv";
+        FXRemoveOldTrashItems = true;
+        NewWindowTarget = "iCloud Drive";
+        ShowExternalHardDrivesOnDesktop = true;
+        ShowHardDrivesOnDesktop = true;
+        ShowMountedServersOnDesktop = true;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+      };
+      iCal = {
+        "TimeZone support enabled" = true;
+        "first day of week" = "Monday";
+      };
+      menuExtraClock = {
+        FlashDateSeparators = true;
+        IsAnalog = false;
+        Show24Hour = true;
+        ShowDate = 0;
+        ShowDayOfMonth = true;
+        ShowDayOfWeek = true;
+        ShowSeconds = true;
+      };
+      screencapture = {
+        location = "~/Desktop/";
+        target = "file";
+      };
+      trackpad = {
+        Clicking = true;
+        FirstClickThreshold = 1;
+        SecondClickThreshold = 1;
+        TrackpadRightClick = true;
+        TrackpadThreeFingerDrag = true;
+      };
+    };
+    keyboard = {
+      enableKeyMapping = true;
+      swapLeftCtrlAndFn = true;
+    };
+    startup.chime = false;
+  };
+
+  time.timeZone = "Africa/Johannesburg";
+
   # The user should already exist, but we need to set this up so Nix knows
   # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
   users.users.francogrobler = {
+    description = "Franco Grobler";
     home = "/Users/francogrobler";
     shell = pkgs.zsh;
   };
