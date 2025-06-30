38034589e508251397d90778b703f579631e0f51
Franco Grobler <franco@grobler.fyi>
2025-06-30 19:25:11 +0200

    fix(nix): enable zsh for default shell

--- nix/users/francogrobler/nixos.nix
+++ nix/users/francogrobler/nixos.nix
@@ -3,6 +3,8 @@
   # Add ~/.local/bin to PATH
   environment.localBinInPath = true;
 
+  programs.zsh.enable = true;
+
   users.users.francogrobler = {
     isNormalUser = true;
     home = "/home/francogrobler";
