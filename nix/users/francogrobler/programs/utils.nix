a665248c93c2f2bd7e27831d7574f479165b888b
Franco Grobler <franco@grobler.fyi>
2025-06-30 19:30:06 +0200

    fix(nix): fix nh config

--- nix/users/francogrobler/programs/utils.nix
+++ nix/users/francogrobler/programs/utils.nix
@@ -2,7 +2,7 @@
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/home/user/my-nixos-config";
+    clean.extraArgs = "--keep-since 7d --keep 2";
+    flake = "~/dotfiles/nix/flake.nix";
   };
 }
