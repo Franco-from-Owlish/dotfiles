fe374aa130786dfd0f3c1d8c8a705bf80c4df14d
Franco Grobler <franco@grobler.fyi>
2025-07-12 15:40:49 +0200

    fix(nvim): remove filetype.nvim

--- nvim/lua/plugins/opentofu.lua
+++ nvim/lua/plugins/opentofu.lua
@@ -17,19 +17,4 @@ return {
       },
     },
   },
-  {
-    "nathom/filetype.nvim",
-    config = function()
-      -- Setup overrides for file extensions
-      require("filetype").setup({
-        overrides = {
-          extensions = {
-            tf = "terraform",
-            tfvars = "terraform",
-            tfstate = "json",
-          },
-        },
-      })
-    end,
-  },
 }
