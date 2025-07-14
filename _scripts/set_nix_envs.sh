3f656bf62b9aa10549740416cc2a646138d3c347
Franco Grobler <franco@grobler.fyi>
2025-07-14 15:33:42 +0200

    feat: ensure just recipe nix-switch will run on all operating systems correctly

---
+++ _scripts/set_nix_envs.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+is_nixos() {
+	# Check if /etc/NixOS/release file exists
+	if [ -f "/etc/NixOS/release" ]; then
+		return 0
+	fi
+
+	# Check if the nixos-version command exists and is executable
+	if command -v nixos-version &>/dev/null; then
+		return 0
+	fi
+
+	# If neither condition is met, it's likely not NixOS
+	return 1
+}
+
+os=$(uname -s)
+if [[ $os == "Darwin" ]]; then
+	export NIXNAME="apple-silicone"
+	export NIXCONFIG="darwinConfigurations"
+elif [[ $os == "Linux" ]]; then
+	export NIXNAME="x86_64-linux"
+	if is_nixos; then
+		export NIXCONFIG="nixosConfigurations"
+	else
+		export NIXCONFIG="homeConfigurations"
+	fi
+else
+	echo "Unsupported OS: $os"
+	exit 1
+fi
