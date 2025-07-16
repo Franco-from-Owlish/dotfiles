913796fad454d3817995425ed643369cb36d344f
Franco Grobler <franco@grobler.fyi>
2025-07-16 21:08:22 +0200

    feat(nix): add wsl machine

---
+++ nix/machines/wsl.nix
@@ -0,0 +1,23 @@
+{ pkgs, currentSystemUser, config, ... }: {
+  imports = [ ];
+
+  wsl = {
+    enable = true;
+    wslConf = {
+      automount = { root = "/mnt"; };
+    };
+    defaultUser = currentSystemUser;
+    startMenuLaunchers = true;
+  };
+
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+      keep-outputs = true
+      keep-derivations = true
+    '';
+  };
+
+  system.stateVersion = config.lib.nixos.stateVersion;
+}
