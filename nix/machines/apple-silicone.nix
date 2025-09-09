b6736f69d986b3288e8996adc5d8148f526c3d7e
Franco Grobler <franco@grobler.fyi>
2025-09-09 21:35:54 +0200

    feat(nix): add denvenv cachix config

--- nix/machines/apple-silicone.nix
+++ nix/machines/apple-silicone.nix
@@ -16,5 +16,10 @@
       keep-outputs = true
       keep-derivations = true
     '';
+
+    settings = {
+      extra-substituters = "https://devenv.cachix.org";
+      extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    };
   };
 }
