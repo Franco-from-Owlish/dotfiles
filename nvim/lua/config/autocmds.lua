4080e00d597cc01137fa5b502255a4229a2f202a
Franco Grobler <franco@grobler.fyi>
2025-11-11 11:33:10 +0200

    feat(nvim): create autocommand to commit lockfile after lazy update

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,44 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Automatically commit lockfile after running Lazy Update (or Sync)
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyUpdate",
+  callback = function()
+    local repo_dir = vim.env.HOME .. "/dotfiles"
+    local lockfile = repo_dir .. "/nvim/lazy-lock.json"
+
+    local cmd = {
+      "git",
+      "-C",
+      repo_dir,
+      "commit",
+      lockfile,
+      "-m",
+      "chore(nvim): update lazy lockfile",
+    }
+
+    local success, process = pcall(function()
+      return vim.system(cmd):wait()
+    end)
+
+    if process and process.code == 0 then
+      vim.notify("Committed lockfile")
+    else
+      if not success then
+        vim.notify("Failed to run command '" .. table.concat(cmd, " ") .. "':", vim.log.levels.ERROR, {})
+      else
+        if process.stderr == "" then
+          vim.notify(
+            process.stdout .. " - exit code " .. process.code,
+            vim.log.levels.WARN, {})
+        else
+          vim.notify(
+            "failed to commit: " .. process.stderr .. " with exit code " .. process.code,
+            vim.log.levels.ERROR, {})
+        end
+      end
+    end
+  end,
+})
